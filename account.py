63c15ff8ba5a9b5be42b0fadf9feb252745042b6
r0meCc <141976266+r0meCc@users.noreply.github.com>
2023-08-10 21:46:39 -0500

    Create account.py
    step 3 in lab 12

---
+++ account.py
@@ -0,0 +1,27 @@
+class Account:
+    def __init__(self, name: str):
+        """Initialize an account with the given name and a balance of 0."""
+        self.__account_name = name
+        self.__account_balance = 0
+
+    def deposit(self, amount: float) -> bool:
+        """Deposit the specified amount into the account balance."""
+        if amount > 0:
+            self.__account_balance += amount
+            return True
+        return False
+
+    def withdraw(self, amount: float) -> bool:
+        """Withdraw the specified amount from the account balance."""
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return True
+        return False
+
+    def get_balance(self) -> float:
+        """Return the current account balance."""
+        return self.__account_balance
+
+    def get_name(self) -> str:
+        """Return the account name."""
+        return self.__account_name
